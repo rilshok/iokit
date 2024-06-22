7b8e596afdf85ee3536144742539aa15f7d4486f
Vladislav A. Proskurov <rilshok@pm.me>
2024-06-22 18:37:13 +0400

    fix: add tar

--- src/iokit/extensions/__init__.py
+++ src/iokit/extensions/__init__.py
@@ -2,10 +2,12 @@ __all__ = [
     "Gzip",
     "Json",
     "Jsonl",
+    "Tar",
     "Txt",
 ]
 
 from .gz import Gzip
 from .json import Json
 from .jsonl import Jsonl
+from .tar import Tar
 from .txt import Txt
