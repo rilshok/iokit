c97fa55d78b708adfde66a15ff422df9c68eba8d
Vladislav A. Proskurov <rilshok@pm.me>
2024-07-29 15:39:31 +0400

    feat: v0.1.4

--- src/iokit/__init__.py
+++ src/iokit/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
     "save_file",
     "save_temp",
 ]
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .extensions import Dat, Env, Gzip, Json, Jsonl, Tar, Txt, Yaml
 from .state import State, filter_states, find_state
