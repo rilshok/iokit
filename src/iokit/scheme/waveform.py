2a2025191a5882a8bb4c756daa84e5f0999e243d
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-28 20:36:55 +0000

    feat: init waveform scheme

---
+++ src/iokit/scheme/waveform.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from numpy.typing import NDArray
+from numpy import float32
+
+
+@dataclass
+class Waveform:
+    wave: NDArray[float32]
+    freq: int
+
+    def copy(self) -> "Waveform":
+        return Waveform(self.wave.copy(), self.freq)
+
+    def cut(self, *, begin: float | None = None, end: float | None = None) -> "Waveform":
+        if begin is None and end is None:
+            return self.copy()
+        raise NotImplementedError()
+
+    def display(self):
+        from IPython.display import Audio, display
+
+        return display(Audio(self.wave, rate=self.freq))
