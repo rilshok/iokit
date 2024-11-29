7b8cf9bee95080c268b98dc2fb8993f8c0d0d819
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-29 12:31:21 +0000

    feat: npy support

---
+++ src/iokit/extensions/npy.py
@@ -0,0 +1,17 @@
+from io import BytesIO
+from typing import Any
+
+import numpy as np
+from numpy.typing import NDArray
+
+from iokit.state import State
+
+
+class Npy(State, suffix="npy"):
+    def __init__(self, array: NDArray[Any], **kwargs: Any) -> None:
+        with BytesIO() as buffer:
+            np.save(buffer, array, allow_pickle=False, fix_imports=False)
+            super().__init__(data=buffer.getvalue(), **kwargs)
+
+    def load(self) -> NDArray[Any]:
+        return np.load(self.data, allow_pickle=False, fix_imports=False)
