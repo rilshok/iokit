d33115c8e8d10963357585f877a33d0fe188dd99
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-29 19:57:41 +0000

    feat: change arguments name

--- src/iokit/extensions/dat.py
+++ src/iokit/extensions/dat.py
@@ -6,8 +6,8 @@ from iokit.state import State
 
 
 class Dat(State, suffix="dat"):
-    def __init__(self, data: bytes, **kwargs: Any):
-        super().__init__(data=data, **kwargs)
+    def __init__(self, blob: bytes, **kwargs: Any):
+        super().__init__(data=blob, **kwargs)
 
     def load(self) -> bytes:
         return self.data
