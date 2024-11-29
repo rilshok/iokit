e9ee983b2e2a65b5a926030e7e71c45cc78adfee
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-29 20:56:28 +0000

    test: check name of state

--- tests/test_table.py
+++ tests/test_table.py
@@ -13,15 +13,19 @@ def _test_teble_data() -> list[dict[str, str | int]]:
 
 def test_csv() -> None:
     frame = pd.DataFrame(_test_teble_data())
-    assert frame.equals(Csv(frame).load())
-    assert Csv(frame, index=True).size > Csv(frame).size
-    assert Csv(frame).load().equals(Csv(frame, index=True).load()[["name", "age"]])
-    assert len([1 for ch in Csv(frame).data if ch == b","[0]]) == 4
+    state = Csv(frame, name="test")
+    assert str(state.name) == "test.csv"
+    assert frame.equals(state.load())
+    assert Csv(frame, index=True).size > state.size
+    assert state.load().equals(Csv(frame, index=True).load()[["name", "age"]])
+    assert len([1 for ch in state.data if ch == b","[0]]) == 4
 
 
 def test_tsv() -> None:
     frame = pd.DataFrame(_test_teble_data())
-    assert frame.equals(Tsv(frame).load())
-    assert Tsv(frame, index=True).size > Tsv(frame).size
-    assert Tsv(frame).load().equals(Tsv(frame, index=True).load()[["name", "age"]])
-    assert len([1 for ch in Tsv(frame).data if ch == b"\t"[0]]) == 4
+    state = Tsv(frame, name="test")
+    assert str(state.name) == "test.tsv"
+    assert frame.equals(state.load())
+    assert Tsv(frame, index=True).size > state.size
+    assert state.load().equals(Tsv(frame, index=True).load()[["name", "age"]])
+    assert len([1 for ch in state.data if ch == b"\t"[0]]) == 4
