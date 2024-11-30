594cb6ba533af7e4ed9c2c1cb6f9e81ad6863b4b
Vladislav A. Proskurov <rilshok@pm.me>
2024-11-30 12:36:21 +0000

    feat: unify hexdigest and verify assertion

--- src/iokit/checksum.py
+++ src/iokit/checksum.py
@@ -4,11 +4,13 @@ import hashlib
 from collections.abc import Generator, Iterator
 from contextlib import contextmanager
 from io import BytesIO
-from typing import Any
+from typing import Any, Literal
 
 import xxhash
 
 CHUNK_SIZE = 4096
+
+HashAlgorithm = Literal["xxh32", "xxh64", "xxh128", "sha256", "md5", "sha1"]
 
 
 @contextmanager
@@ -54,25 +56,48 @@ def _hexdigest(algorithm: Any, data: Any) -> str:
 
 class ChecksumMixin:
     @property
-    def hexdigest_xxh32(self) -> str:
+    def _hexdigest_xxh32(self) -> str:
         return _hexdigest(algorithm=xxhash.xxh32(), data=self)
 
     @property
-    def hexdigest_xxh64(self) -> str:
+    def _hexdigest_xxh64(self) -> str:
         return _hexdigest(algorithm=xxhash.xxh64(), data=self)
 
     @property
-    def hexdigest_xxh128(self) -> str:
+    def _hexdigest_xxh128(self) -> str:
         return _hexdigest(algorithm=xxhash.xxh128(), data=self)
 
     @property
-    def hexdigest_sha256(self) -> str:
+    def _hexdigest_sha256(self) -> str:
         return _hexdigest(algorithm=hashlib.sha256(), data=self)
 
     @property
-    def hexdigest_md5(self) -> str:
+    def _hexdigest_md5(self) -> str:
         return _hexdigest(algorithm=hashlib.md5(), data=self)
 
     @property
-    def hexdigest_sha1(self) -> str:
+    def _hexdigest_sha1(self) -> str:
         return _hexdigest(algorithm=hashlib.sha1(), data=self)
+
+    def hexdigest(self, algorithm: HashAlgorithm = "xxh64") -> str:
+        match algorithm:
+            case "xxh32":
+                return self._hexdigest_xxh32
+            case "xxh64":
+                return self._hexdigest_xxh64
+            case "xxh128":
+                return self._hexdigest_xxh128
+            case "sha256":
+                return self._hexdigest_sha256
+            case "md5":
+                return self._hexdigest_md5
+            case "sha1":
+                return self._hexdigest_sha1
+            case other:
+                msg = f"Unknown hash algorithm '{other}'"
+                raise ValueError(msg)
+
+    def hexdigest_assert(self, algorithm: HashAlgorithm, hexdigest: str) -> None:
+        if (checksum := self.hexdigest(algorithm)) != hexdigest:
+            msg = f"Expected {algorithm} {hexdigest =}, got ={checksum!r}"
+            raise AssertionError(msg)
