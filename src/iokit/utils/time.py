da577f38f140866966b5d0b96498e61b8e59a41d
Vladislav A. Proskurov <rilshok@pm.me>
2026-08-02 16:26:41 +0400

    feat: timestamp class

--- src/iokit/utils/time.py
+++ src/iokit/utils/time.py
@@ -1,11 +1,28 @@
-from datetime import datetime
-
-from pytz import utc
+from datetime import UTC, datetime, timedelta
+from typing import Self
 
 
 def fromtimestamp(timestamp: float) -> datetime:
-    return datetime.fromtimestamp(timestamp, utc)
+    return datetime.fromtimestamp(timestamp, UTC)
 
 
 def now() -> datetime:
-    return datetime.now(utc)
+    return datetime.now(UTC)
+
+
+class Timestamp(int):
+    def __new__(cls, dt: int | None = None) -> Self:
+        if dt is None:
+            dt = int(now().timestamp())
+        return super().__new__(cls, dt)
+
+    @classmethod
+    def from_dt(cls, dt: datetime) -> Self:
+        return cls(int(dt.timestamp()))
+
+    @property
+    def dt(self) -> datetime:
+        return datetime.fromtimestamp(self, tz=UTC)
+
+    def shift(self, td: timedelta) -> int:
+        return Timestamp.from_dt(self.dt + td)
