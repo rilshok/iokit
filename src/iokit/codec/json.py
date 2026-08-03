3a96d9f8c80b8cfcdf4c57e0cadc5ad6c8f567da
Vladislav A. Proskurov <rilshok@pm.me>
2026-08-03 08:16:29 +0400

    feat: json engine

---
+++ src/iokit/codec/json.py
@@ -0,0 +1,33 @@
+import json
+from io import BytesIO
+from typing import Any, BinaryIO
+
+from iokit.utils.state_new import Codec, Pattern
+
+D = dict[str, Any] | list[Any] | str
+
+
+class JsonCodec(Codec[D]):
+    keys = (Pattern("*.json"),)
+
+    def __init__(
+        self,
+        *,
+        compact: bool = False,
+        ensure_ascii: bool = False,
+        allow_nan: bool = False,
+    ) -> None:
+        item_sep = "," if compact else ", "
+        key_sep = ":" if compact else ": "
+        self._dumps = json.JSONEncoder(
+            ensure_ascii=ensure_ascii,
+            allow_nan=allow_nan,
+            sort_keys=False,
+            separators=(item_sep, key_sep),
+        ).encode
+
+    def encode(self, data: D) -> BytesIO:
+        return BytesIO(self._dumps(data).encode("utf-8"))
+
+    def decode(self, buffer: BinaryIO) -> D:
+        return json.load(buffer)
