95aa5ba9e1f96caec9b40ed321d02630d8821e66
Vladislav A. Proskurov <rilshok@pm.me>
2026-08-06 02:59:51 +0400

    style: add state annotation:

--- src/iokit/codec/tar.py
+++ src/iokit/codec/tar.py
@@ -3,20 +3,20 @@ __all__ = ["TarCodec"]
 import tarfile
 from collections.abc import Iterable
 from io import BytesIO
-from typing import BinaryIO
+from typing import Any, BinaryIO
 
 from iokit.codec.base import Codec
 from iokit.state import BufferedState, LoadedState, State
 
 
-class TarCodec(Codec[Iterable[State]]):
+class TarCodec(Codec[Iterable[State[Any]]]):
     def __init__(self, *, buffered: bool = False) -> None:
         self._buffered = buffered
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}(buffered={self._buffered})"
 
-    def encode(self, data: Iterable[State]) -> BytesIO:
+    def encode(self, data: Iterable[State[Any]]) -> BytesIO:
         buffer = BytesIO()
         with tarfile.open(fileobj=buffer, mode="w") as archive:
             for state in data:
@@ -27,7 +27,7 @@ class TarCodec(Codec[Iterable[State]]):
         buffer.seek(0)
         return buffer
 
-    def decode(self, buffer: BinaryIO) -> Iterable[State]:
+    def decode(self, buffer: BinaryIO) -> Iterable[State[Any]]:
         with buffer, tarfile.open(fileobj=buffer, mode="r") as archive:
             for member in archive.getmembers():
                 if not member.isfile():
