1ea593e815c03bbcca16aa744b4f162ff635f804
Vladislav A. Proskurov <rilshok@pm.me>
2026-08-06 02:35:28 +0400

    feat: simplify pattern

--- src/iokit/utils/pattern.py
+++ src/iokit/utils/pattern.py
@@ -1,7 +1,5 @@
 from fnmatch import fnmatch
 
-WRAPPER_PREFIX = "*.*"
-
 
 class Pattern(str):
     def __len__(self) -> int:
@@ -9,12 +7,3 @@ class Pattern(str):
 
     def __call__(self, string: str) -> bool:
         return fnmatch(name=string, pat=str(self))
-
-    @property
-    def wrapper(self) -> bool:
-        """Whether the pattern, like `*.*.gz`, describes a container around another format."""
-        return self.startswith(WRAPPER_PREFIX)
-
-    def unwrap(self, name: str) -> str:
-        """Strip the container suffix, leaving the name of whatever the container holds."""
-        return name.removesuffix(self.removeprefix(WRAPPER_PREFIX))
