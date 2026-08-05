30f6525328639b6deea9b7e65c73aa8bd83fe59e
Vladislav A. Proskurov <rilshok@pm.me>
2026-08-06 03:54:18 +0400

    test: rename download testing

--- tests/test_download_file.py
+++ tests/test_download_file.py
@@ -1,7 +1,7 @@
 from iokit.utils.web import download
 
 
-def test_download_file() -> None:
+def test_download() -> None:
     uri = "https://raw.githubusercontent.com/rilshok/iokit/main/LICENSE"
     state = download(uri)
     assert "MIT License" in state.data.decode("utf-8")
